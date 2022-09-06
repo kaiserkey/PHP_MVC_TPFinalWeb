a561050318558cbad05d2ddc899806684d3458ac
DanielSan <56778742+kaiserkey@users.noreply.github.com>
2022-09-06 18:11:57 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?
+#direccion absoluta del proyecto en localhost, es necesario cambiarla si se pasa a otra maquina
+define('URL', 'http://localhost/MVC-TPFinalWebII/');
+
+define('HOST', 'localhost');
+define('DB', 'gestion_turnos');
+define('USER', 'root');
+define('PASS', '');
+define('CHARSET', 'utf8mb4');
+?>
